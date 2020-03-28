1a9e8eb3e029b8ef9b83c91cff11f93eedb18859
Felipe Groberio <teixeirf@oregonstate.edu>
2020-03-28 17:10:21 -0500

    new images, itemList updated

--- js/itemsList.js
+++ js/itemsList.js
@@ -3,10 +3,15 @@
  */
 
 var itemsList = [
-    { name: 'toilet_paper', displayName: 'Toilet Paper', photo: 'images/toilet_paper.jpg' },
-    { name: 'gloves', displayName: 'Gloves', photo: 'images/gloves.jpg' },
-    { name: 'hand_sanitizer', displayName: 'Hand Sanitizer', photo: 'images/hand_sanitizer.jpg' },
-    { name: 'paper_towel', displayName: 'Paper Towel', photo: 'images/paper_towel.jpg' }
+    { name: 'toilet_paper',     displayName: 'Toilet Paper',    photo: 'images/toilet_paper.jpg' },
+    { name: 'gloves',           displayName: 'Gloves',          photo: 'images/gloves.jpg' },
+    { name: 'hand_sanitizer',   displayName: 'Hand Sanitizer',  photo: 'images/hand_sanitizer.jpg' },
+    { name: 'clorox',           displayName: 'Clorox',          photo: 'images/clorox.jpg' },
+    { name: 'clorox_wipes',     displayName: 'Clorox Wipes',    photo: 'images/clorox_wipes.jpg' },
+    { name: 'face_mask',        displayName: 'Face Mask',       photo: 'images/face_mask.jpg' },
+    { name: 'water',            displayName: 'Water',           photo: 'images/water.jpg' },
+    { name: 'lysol',            displayName: 'Lysol',           photo: 'images/Lysol.jpg' },
+    { name: 'paper_towel',      displayName: 'Paper Towel',     photo: 'images/paper_towel.jpg' }
 ];
     
 module.exports = itemsList;
