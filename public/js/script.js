62bc048e946644484b78a8e9dfe6206c4b4a0959
Cameron Grover <camdgrover@gmail.com>
2020-03-28 15:22:19 -0500

    Added submitForm onclick Added hidden forms for div item names Added form field requirements Styling borders

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,7 @@ const $ = jQuery;
 let giveCards = $('#give .card');
 let needCards = $('#need .card');
 
+
 window.addEventListener('load', () => {
     giveCards.each(function() {
         $(this).click(function () {
@@ -17,3 +18,35 @@ window.addEventListener('load', () => {
         });
     });
 });
+
+function submitForm(event) {
+    event.preventDefault();
+
+    // Validate fields
+    let form = document.querySelector('form')
+    form.reportValidity();
+
+    let data = {};
+    data.name = $('#uname').val();
+    data.location = $('#zip').val();
+
+    let giveSelected = $('#give .selected input');
+    let needSelected = $('#need .selected input');
+
+    if (giveSelected.length < 1 && needSelected.length < 1) {
+        alert('Please select at least one item');
+        return;
+    }
+
+    data.give = [];
+    giveSelected.each(function () {
+        data.give.push($(this).val());
+    });
+
+    data.need = [];
+    needSelected.each(function () {
+        data.need.push($(this).val());
+    });
+    console.log(data);
+
+}
